8cfae59702bf91d992c2a4966781d636cbad6f3e
Ramir1914 <ramirsakatov@abv.bg>
2024-12-13 09:08:03 +0200

    Add files via upload

---
+++ CHISLA/CHISLA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHISLA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kak se kazvash");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Zdravei, {name}!");
+            Console.Write("Kak e imeto na nomer 17");
+            string name1 = Console.ReadLine();
+            Console.WriteLine($"Moje bi {name1} shte se opita da zavurshi");
+
+
+        }
+    }
+}
